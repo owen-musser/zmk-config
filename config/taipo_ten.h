cd34a5d1845a6bcadce3725a82f7e23cb2238924
owen <omusse1@wgu.edu>
2025-07-14 20:30:04 -0400

    starting combos

---
+++ config/taipo_ten.h
@@ -0,0 +1,23 @@
+#pragma once
+
+// LAYERS
+#define MACOS 0
+#define GAMING 1
+
+// TIMEOUTS
+#define TIMEOUT_SMALL 80
+#define TIMEOUT_NORMAL 100
+#define TIMEOUT_LONG 200
+#define TIMEOUT_XLONG 1500
+
+// KEYS
+#define K_A 0
+#define K_O 1
+#define K_T 2
+#define K_E 3
+#define K_BS 4
+#define K_R 5
+#define K_S 6
+#define K_N 7
+#define K_I 8
+#define K_S 9
